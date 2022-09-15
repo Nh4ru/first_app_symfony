bf1ce1e3c11a28c8c72a75b093b39e69a69925f5
Nh4ru <tristanfernandez@live.fr>
2022-09-15 11:56:25 +0200

    feat(register): add register page

--- src/Controller/Frontend/CompteController.php
+++ src/Controller/Frontend/CompteController.php
@@ -2,21 +2,51 @@
 
 namespace App\Controller\Frontend;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Security;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CompteController extends AbstractController
 {
+    public function __construct(
+        private Security $security,
+        private UserRepository $userRepository
+    ) {
+    }
 
-    #[Route('/compte', name: 'app_compte')]
+    #[Route('/compte', name: 'app.user.compte')]
     public function indexCompte(Security $security): Response
     {
         $user = $security->getUser();
 
         return $this->render('Frontend/User/compte.html.twig', [
             'user' => $user,
+        ]);
+    }
+
+    #[Route('/compte/edit', name: 'app.user.edit', methods: ['GET', 'POST'])]
+    public function editAccount(Request $request): Response|RedirectResponse
+    {
+        $user = $this->security->getUser();
+
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->userRepository->add($user, true);
+            $this->addFlash('success', 'Votre compte a été modifier avec succès');
+
+            return $this->redirectToRoute('app.user.compte');
+        }
+
+        return $this->renderForm('Backend/User/edit.html.twig', [
+            'user' => $user,
+            'form' => $form
         ]);
     }
 }
