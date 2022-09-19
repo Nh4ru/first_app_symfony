78086725c4f8e126261b8eff96abc2767c3f5b08
Nh4ru <tristanfernandez@live.fr>
2022-09-19 16:18:24 +0200

    feat(commentaire): add new feature Comment on article

--- src/Controller/Frontend/ArticleController.php
+++ src/Controller/Frontend/ArticleController.php
@@ -3,6 +3,11 @@
 namespace App\Controller\Frontend;
 
 use App\Entity\Article;
+use App\Entity\Comment;
+use App\Form\CommentType;
+use App\Repository\CommentRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -11,8 +16,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 #[Route('/article')]
 class ArticleController extends AbstractController
 {
-    #[Route('/details/{slug}', name: 'app.article.details', methods: ['GET'])]
-    public function showArticle(?Article $article): Response|RedirectResponse
+    #[Route('/details/{slug}', name: 'app.article.details', methods: ['GET', 'POST'])]
+    public function showArticle(?Article $article, Request $request, Security $security, CommentRepository $repoComment): Response|RedirectResponse
     {
         if (!$article instanceof Article) {
             $this->addFlash('error', 'Article non trouvé');
@@ -20,8 +25,26 @@ class ArticleController extends AbstractController
             return $this->redirectToRoute('home');
         }
 
-        return $this->render('Frontend/Article/show.html.twig', [
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment
+                ->setUser($security->getUser())
+                ->setActive(true)
+                ->setArticle($article);
+            $repoComment->add($comment, true);
+
+            $this->addFlash('success', 'Commentaire posté');
+            return $this->redirectToRoute('app.article.details', [
+                'slug' => $article->getSlug()
+            ], 301);
+        }
+
+        return $this->renderForm('Frontend/Article/show.html.twig', [
             'article' => $article,
+            'form' => $form,
         ]);
     }
 }
