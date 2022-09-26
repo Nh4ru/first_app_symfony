6cc66cbe7ec76974e21c756b9c0d04227084ba08
Nh4ru <tristanfernandez@live.fr>
2022-09-26 09:05:24 +0200

    feat(scss): modify elements in scss

--- migrations/Version20220925174140.php
+++ migrations/Version20220925174140.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220924131636 extends AbstractMigration
+final class Version20220925174140 extends AbstractMigration
 {
     public function getDescription(): string
     {
